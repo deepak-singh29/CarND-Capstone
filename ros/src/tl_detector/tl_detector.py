88aa47323919c974b6b33bc6154299860f5efb9f
deepak-singh29 <deepak.singh513@gmail.com>
2018-07-05 06:50:58 +0000

    Tl change

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -7,6 +7,7 @@ from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
+from scipy.spatial import KDTree
 import tf
 import cv2
 import yaml
@@ -101,7 +102,9 @@ class TLDetector(object):
 
         """
         # implement
-        closest_idx = self.waypoint_tree.query([x,y],1)[1]
+	waypoints_2d = [[wp.pose.pose.position.x,wp.pose.pose.position.y] for wp in self.waypoints.waypoints]
+	waypoints_tree = KDTree(waypoints_2d)
+        closest_idx = waypoints_tree.query([x,y],1)[1]
         return closest_idx
 
     def get_light_state(self, light):
@@ -157,7 +160,7 @@ class TLDetector(object):
                     line_wp_idx = temp_wp_idx
         if closest_light:
             state = self.get_light_state(closest_light)
-            return light_wp_idx, state
+            return line_wp_idx, state
         self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
